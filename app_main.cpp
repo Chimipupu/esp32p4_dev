9b64e9bd73bbb63c953bb80beb1cd6660242f4fb
Chimipupu <chiminyanyan@gmail.com>
2026-08-22 17:54:23 +0900

    ログの文字列をログタスクにキューで送信するまでを関数化

--- app_main.cpp
+++ app_main.cpp
@@ -26,6 +26,8 @@ static const char *g_wifi_password = MY_WIFI_PASSWORD;
 // ---------------------------------------------------
 // [DEBUG関連]
 static void _dbg_pcb_info_print(void);
+QueueHandle_t g_queue_handle_log = NULL;
+static void DBG_LOG_PRINT(QueueHandle_t queue_handle, const char *p_msg, ...);
 
 // ---------------------------------------------------
 // [グローバル]
@@ -54,7 +56,6 @@ static xTaskHandle s_xTaskCore0;
 static xTaskHandle s_xTaskCore1;
 static xTaskHandle s_xTaskDebugLog;
 QueueHandle_t g_queue_handle_core2core = NULL;
-QueueHandle_t g_queue_handle_log = NULL;
 
 static void vTaskCore0(void *p_param);
 static void vTaskCore1(void *p_param);
@@ -65,6 +66,18 @@ static void _mcu_init(void);
 static void _pcb_init(void);
 // ---------------------------------------------------
 // [Static関数]
+
+static void DBG_LOG_PRINT(QueueHandle_t queue_handle, const char *p_msg, ...)
+{
+    log_msg_t log_msg;
+    va_list args;
+
+    va_start(args, p_msg);
+    vsnprintf(log_msg.msg, sizeof(log_msg.msg), p_msg, args);
+    va_end(args);
+
+    xQueueSend(queue_handle, &log_msg, 0);
+}
 
 static void _mcu_init(void)
 {
@@ -125,30 +138,29 @@ static void _pcb_init(void)
 static void vTaskCore0(void *p_param)
 {
     BaseType_t que_status;
-    log_msg_t log_msg;
     static uint8_t s_core_num = xPortGetCoreID();
 
-    snprintf(log_msg.msg, sizeof(log_msg.msg), "[CPU Core %d] vTaskCore1\n", s_core_num);
-    xQueueSend(g_queue_handle_log, &log_msg, 0);
+    DBG_LOG_PRINT(g_queue_handle_log, "[CPU Core %d] vTaskCore0\n", s_core_num);
 
     while (1)
     {
         que_status = xQueueReceive(g_queue_handle_core2core,
                                     &s_local_rgb_led_val,
                                     portMAX_DELAY);
+
         if(que_status == pdPASS)
         {
 #ifdef RGBLED_USE
             app_neopixel_set_rgb(0, &s_local_rgb_led_val);
-            snprintf(log_msg.msg, sizeof(log_msg.msg),
+            DBG_LOG_PRINT(g_queue_handle_log,
                     "[CPU Core %d] RGB_LED: Set Color = (0x%02X, 0x%02X, 0x%02X)\n",
                     s_core_num,
                     s_local_rgb_led_val.para.red,
                     s_local_rgb_led_val.para.green,
                     s_local_rgb_led_val.para.blue);
-            xQueueSend(g_queue_handle_log, &log_msg, 0);
 #endif
         }
+
         vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
@@ -156,13 +168,11 @@ static void vTaskCore0(void *p_param)
 static void vTaskCore1(void *p_param)
 {
     BaseType_t que_status;
-    log_msg_t log_msg;
 
     static uint8_t s_tbl_idx = 0;
     static uint8_t s_core_num = xPortGetCoreID();
 
-    snprintf(log_msg.msg, sizeof(log_msg.msg), "[CPU Core %d] vTaskCore1\n", s_core_num);
-    xQueueSend(g_queue_handle_log, &log_msg, 0);
+    DBG_LOG_PRINT(g_queue_handle_log, "[CPU Core %d] vTaskCore1\n", s_core_num);
 
     while (1)
     {
@@ -174,18 +184,18 @@ static void vTaskCore1(void *p_param)
         // キューの送信完了を受けて次のデータを用意しておく
         if(que_status == pdPASS)
         {
-            snprintf(log_msg.msg, sizeof(log_msg.msg),
+            DBG_LOG_PRINT(g_queue_handle_log,
                     "[CPU Core %d] RGB_LED: Request Color = (0x%02X, 0x%02X, 0x%02X)\n",
                     s_core_num,
                     s_request_rgb_led_val.para.red,
                     s_request_rgb_led_val.para.green,
                     s_request_rgb_led_val.para.blue);
-            xQueueSend(g_queue_handle_log, &log_msg, 0);
 
             s_request_rgb_led_val.rgb = g_led_color_tbl[s_tbl_idx].rgb.rgb;
             s_tbl_idx = (s_tbl_idx + 1) % (RGBLED_COLOR_TBL_SIZE - 1);
         }
 #endif
+
         vTaskDelay(200 / portTICK_PERIOD_MS);
     }
 }
@@ -194,7 +204,6 @@ static void vTaskDebugLog(void *p_param)
 {
     BaseType_t que_status;
     log_msg_t log_msg;
-    // static uint8_t s_cpu_core = xPortGetCoreID();
 
     while (1)
     {
@@ -208,17 +217,27 @@ static void vTaskDebugLog(void *p_param)
             memset(&log_msg.msg[0], 0x00, sizeof(log_msg.msg));
         }
 
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        vTaskDelay(20 / portTICK_PERIOD_MS);
     }
 }
 
 static void _freertos_init(void)
 {
     // キューの作成
-    g_queue_handle_log = xQueueCreate(16, sizeof(log_msg_t));
+    g_queue_handle_log = xQueueCreate(8, sizeof(log_msg_t));
 #ifdef RGBLED_USE
     g_queue_handle_core2core = xQueueCreate(8, sizeof(led_color_t));
 #endif
+
+    // [デバッグ用のログをprintf()するだけのタスク @CPU Core 1]
+    xTaskCreatePinnedToCore(vTaskDebugLog,     // コールバック関数ポインタ
+                            "vTaskDebugLog",   // タスク名
+                            4096,              // スタック
+                            NULL,              // パラメータ
+                            0,                 // 優先度(0～7、7が最優先)
+                            &s_xTaskDebugLog,  // ハンドル
+                            APP_PROC_CORE      // CPUのコア選択
+                            );
 
     // [RTOSタスク @CPU Core 0]
     xTaskCreatePinnedToCore(vTaskCore0,        // コールバック関数ポインタ
@@ -249,16 +268,6 @@ static void _freertos_init(void)
                             NULL,              // パラメータ
                             2,                 // 優先度(0～7、7が最優先)
                             &s_xTaskCore1,     // ハンドル
-                            APP_PROC_CORE      // CPUのコア選択
-                            );
-
-    // [デバッグ用のログをprintf()するだけのタスク @CPU Core 1]
-    xTaskCreatePinnedToCore(vTaskDebugLog,     // コールバック関数ポインタ
-                            "vTaskDebugLog",   // タスク名
-                            4096,              // スタック
-                            NULL,              // パラメータ
-                            0,                 // 優先度(0～7、7が最優先)
-                            &s_xTaskDebugLog,     // ハンドル
                             APP_PROC_CORE      // CPUのコア選択
                             );
 }
